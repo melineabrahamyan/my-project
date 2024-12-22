d0fb91c579b0d166ad12e26cac1fc669e31d606b
melineabrahamyan <abrahamyan.meline@gmail.com>
2024-12-22 23:35:19 +0400

    updated eslint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -13,7 +13,11 @@ const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
   {
     rules: {
-      "@typescript-eslint/no-explicit-any": "off", 
+      "@typescript-eslint/no-explicit-any": "off",
+      "@typescript-eslint/no-unused-vars": "off",
+      "no-unused-vars": "off",
+      "@typescript-eslint/no-unused-expressions": "off",
+      "no-unused-expressions": "off",
     },
   },
 ];
